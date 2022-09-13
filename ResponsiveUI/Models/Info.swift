d3d1dfc5e3fa1d32a90b96b85b274ddb9efe9116
Adarsh Shukla <shuklaadarsh1932@gmail.com>
2022-09-13 10:58:34 +0530

    project setup

---
+++ ResponsiveUI/Models/Info.swift
@@ -0,0 +1,27 @@
+//
+//  Info.swift
+//  ResponsiveUI
+//
+//  Created by Adarsh Shukla on 13/09/22.
+//
+
+import SwiftUI
+
+//MARK: Info Cards and Sample Data
+
+struct Info: Identifiable {
+    var id: String = UUID().uuidString
+    var title: String
+    var amount: String
+    var percentage: Int
+    var loss: Bool = false
+    var icon: String
+    var iconColor: Color
+}
+
+var infos: [Info] = [
+    Info(title: "Revenue", amount: "2.047", percentage: 10, icon: "arrow.up.right", iconColor: .orange),
+    Info(title: "Orders", amount: "356", percentage: 20, icon: "cart", iconColor: .green),
+    Info(title: "Dine In", amount: "220", percentage: 10, icon: "fork.knife", iconColor: .red),
+    Info(title: "Take away", amount: "135", percentage: 5, loss: true, icon: "takeoutbag.and.cup.and.straw.fill", iconColor: .yellow)
+]
---
+++ ResponsiveUI/Views/ResponsiveView.swift
@@ -0,0 +1,21 @@
+//
+//  ResponsiveView.swift
+//  ResponsiveUI
+//
+//  Created by Adarsh Shukla on 13/09/22.
+//
+
+import SwiftUI
+
+//MARK: Custom View which will give useful properties for adaptive UI
+struct ResponsiveView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ResponsiveView_Previews: PreviewProvider {
+    static var previews: some View {
+        ResponsiveView()
+    }
+}
