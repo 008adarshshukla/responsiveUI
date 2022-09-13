d60c790ca553b2bbdd8ef1986e5b37b3ababa991
Adarsh Shukla <shuklaadarsh1932@gmail.com>
2022-09-13 12:00:39 +0530

    Built Custom responsive View Wrapper

---
+++ ResponsiveUI/Views/Home.swift
@@ -0,0 +1,20 @@
+//
+//  Home.swift
+//  ResponsiveUI
+//
+//  Created by Adarsh Shukla on 13/09/22.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
--- ResponsiveUI/Views/ResponsiveView.swift
+++ ResponsiveUI/Views/ResponsiveView.swift
@@ -8,14 +8,46 @@
 import SwiftUI
 
 //MARK: Custom View which will give useful properties for adaptive UI
-struct ResponsiveView: View {
+struct ResponsiveView<Content: View>: View {
+    
+    var content: (Properties) -> Content
+    init(@ViewBuilder content: @escaping (Properties) -> Content) {
+        self.content = content
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { proxy in
+            let size = proxy.size
+            let isLandscape = size.width > size.height
+            let isIPad = UIDevice.current.userInterfaceIdiom == .pad
+            let isMaxSplit = isSplit() && size.width < 400
+            
+            let properties = Properties(isLandscape: isLandscape, isiPad: isIPad, isSplit: isSplit(), isMaxSplit: isMaxSplit, size: size)
+            
+            content(properties)
+                .frame(width: size.width, height: size.height)
+        }
+    }
+    
+    //MARK: Simple way to find if the app is in Split View.
+    func isSplit() -> Bool {
+        guard let screen = UIApplication.shared.connectedScenes.first as? UIWindowScene else { return false }
+        
+        return screen.windows.first?.frame.size != screen.screen.bounds.size
     }
 }
 
 struct ResponsiveView_Previews: PreviewProvider {
     static var previews: some View {
-        ResponsiveView()
+        ContentView()
     }
+}
+
+struct Properties {
+    var isLandscape: Bool
+    var isiPad: Bool
+    var isSplit: Bool
+    //MARK: if the app size is reduced to more than 1/3 in split mode on iPad
+    var isMaxSplit: Bool
+    var size: CGSize
 }
